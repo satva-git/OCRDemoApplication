7461e96592dd2191aa309e56fe7e560ce4058274
jignasha240891 <121225453+jignasha240891@users.noreply.github.com>
2023-01-02 23:52:25 +0530

    Update BundleConfig.cs

--- OCRDemoApplication/App_Start/BundleConfig.cs
+++ OCRDemoApplication/App_Start/BundleConfig.cs
@@ -8,6 +8,7 @@ namespace OCRDemoApplication
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            // Test
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
