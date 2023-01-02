ccd68154030409693cd19d76f624dd7623253ec5
jignasha240891 <121225453+jignasha240891@users.noreply.github.com>
2023-01-03 00:03:27 +0530

    Added one file

--- OCRDemoApplication/Models/ApiResult.cs
+++ OCRDemoApplication/Models/ApiResult.cs
@@ -5,6 +5,7 @@ namespace OCRDemoApplication.Models
     public class ApiResult
     {
         public object Data { get; set; }
-        public List<Dictionary<string, string>> DataDictionary { get; set; }
+        public List<Dictionary<string, string>> DataDictionary1 { get; set; }
+        public List<Dictionary<string, string>> DataDictionary2 { get; set; }
     }
 }
