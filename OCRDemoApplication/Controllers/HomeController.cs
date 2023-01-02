173bffb41c8e73182fdd084323ccc6207fe136bc
jignasha240891 <121225453+jignasha240891@users.noreply.github.com>
2023-01-02 23:51:15 +0530

    Jigna - Test Commit

--- OCRDemoApplication/Controllers/HomeController.cs
+++ OCRDemoApplication/Controllers/HomeController.cs
@@ -11,22 +11,33 @@ namespace OCRDemoApplication.Controllers
 {
     public class HomeController : BaseController
     {
+        // Test
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult ViewFileData(string fileName)
+        public async Task<ActionResult> ViewFileData(string fileName)
         {
             try
             {
+                // test commit
                 ITextractService _TextractService = new TextractService();
-                fileName = sampleFileFolderPath + fileName;
                 ApiResult apiResult = new ApiResult();
-                // async Task<ActionResult> 
-                // await _TextractService.SaveFileToBucketAndGetResponse(fileName, apiResult);
-                // apiResult.DataDictionary = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>
-                //    (JsonConvert.SerializeObject(apiResult.Data));
+                // async Task<ActionResult> - change return type and uncomment the below code to make working
+
+                var incomeStatementResult = SplitPDFPage(fileName, 1);
+                await _TextractService.SaveFileToBucketAndGetResponse(sampleFileFolderPath + incomeStatementResult.NewFileName, apiResult);
+                apiResult.DataDictionary1 = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>
+                   (JsonConvert.SerializeObject(apiResult.Data));
+                DeleteFile(incomeStatementResult.NewFileName);
+
+                var balanceSheetResult = SplitPDFPage(fileName, 4);
+                await _TextractService.SaveFileToBucketAndGetResponse(sampleFileFolderPath + balanceSheetResult.NewFileName, apiResult);
+                apiResult.DataDictionary2 = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>
+                   (JsonConvert.SerializeObject(apiResult.Data));
+                DeleteFile(balanceSheetResult.NewFileName);
+
                 return View(apiResult);
             }
             catch (Exception ex)
@@ -65,28 +76,36 @@ namespace OCRDemoApplication.Controllers
         {
             try
             {
-                string filename = sampleFileFolderPath + fileName;
-                PdfDocument pdfDocument = PdfReader.Open(filename, PdfDocumentOpenMode.Import);
-
-                PdfDocument outputDocument = new PdfDocument();
-                outputDocument.Version = pdfDocument.Version;
-                outputDocument.Info.Title = "Splitted_PDF_" + GetUniqueFileName();
-                outputDocument.Info.Creator = pdfDocument.Info.Creator;
-                outputDocument.AddPage(pdfDocument.Pages[0]);
-
-                var newFileName = "Splitted_PDF_" + GetUniqueFileName() + ".pdf";
-                outputDocument.Save(sampleFileFolderPath + newFileName);
-
-                byte[] newFileBytes = GetBytes(newFileName);
-                DeleteFile(newFileName);
-
-                return File(newFileBytes, "application/pdf");
+                var updatedFileResult = SplitPDFPage(fileName, 1);
+                DeleteFile(updatedFileResult.NewFileName);
+                return File(updatedFileResult.Bytes, "application/pdf");
             }
             catch (Exception ex)
             {
                 TempData["ErrorMessage"] = ex;
                 throw ex;
             }
+        }
+
+        private UpdatedFileResult SplitPDFPage(string fileName, int pageNo)
+        {
+            UpdatedFileResult updatedFileResult = new UpdatedFileResult();
+            string filename = sampleFileFolderPath + fileName;
+            PdfDocument pdfDocument = PdfReader.Open(filename, PdfDocumentOpenMode.Import);
+
+            PdfDocument outputDocument = new PdfDocument();
+            outputDocument.Version = pdfDocument.Version;
+            outputDocument.Info.Title = "Splitted_PDF_" + GetUniqueFileName();
+            outputDocument.Info.Creator = pdfDocument.Info.Creator;
+            outputDocument.AddPage(pdfDocument.Pages[pageNo - 1]);
+
+            var newFileName = "Splitted_PDF_" + GetUniqueFileName() + ".pdf";
+            outputDocument.Save(sampleFileFolderPath + newFileName);
+
+            updatedFileResult.NewFileName = newFileName;
+            updatedFileResult.Bytes = GetBytes(newFileName);
+
+            return updatedFileResult;
         }
     }
 }
